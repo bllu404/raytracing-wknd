dea80164a72f76365199c49030150ecb1b89efc6
bllu404 <96asdffdsa69@protonmail.com>
2023-11-13 22:58:26 +0200

    cooler scene

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ use std::rc::Rc;
 use crate::camera::Camera;
 use crate::color::Color;
 use crate::hittable::{HittableList, Sphere};
-use crate::material::{Lambertian, Material};
+use crate::material::{Lambertian, Material, Metal};
 use crate::vec::Point3;
 
 fn main() {
@@ -20,10 +20,17 @@ fn main() {
 
     let mat: Rc<dyn Material> = Rc::new(Lambertian{albedo: Color::new(0.3, 1.0, 0.3)});
 
-    world.push(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, Rc::clone(&mat))));
-    world.push(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, Rc::clone(&mat))));
+    let material_ground: Rc<dyn Material> = Rc::new(Lambertian{albedo: Color::new(0.8, 0.8, 0.0)});
+    let material_center: Rc<dyn Material> = Rc::new(Lambertian{albedo: Color::new(0.7, 0.3, 0.3)});
+    let material_left: Rc<dyn Material> = Rc::new(Metal{albedo: Color::new(0.8, 0.8, 0.8)});
+    let material_right: Rc<dyn Material> = Rc::new(Metal{albedo: Color::new(0.8, 0.6, 0.2)});
 
-    let cam = Camera::new(16.0 / 9.0, 400);
+    world.push(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, material_ground)));
+    world.push(Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, material_center)));
+    world.push(Box::new(Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, material_left)));
+    world.push(Box::new(Sphere::new(Point3::new(1.0, 0.0, -1.0), 0.5, material_right)));
+
+    let cam = Camera::new(16.0 / 9.0, 800);
 
     cam.render(&world);
 }
